a5d45d4b1c3dda38f6c6f2fc003cc8d868bdfa7f
Alan Lekah <alekah@live.com>
2016-09-15 14:14:46 -0400

    Update CopyTestForThreading.py

--- PDF Compression Module/CopyTestForThreading.py
+++ PDF Compression Module/CopyTestForThreading.py
@@ -1,7 +1,7 @@
 import shutil, random, time
 
 def randomCopy():
-    src_file = "C:\\Documents and Settings\\Administrator\\Desktop\\ANDRIA_MAXWELL_MAY_28_2015.pdf"
+    src_file = ""
     dest = ("T:\\reception 1\\%d.pdf" % (random.randint(1,25)))
     shutil.copy(src_file, dest)
     time.sleep(10)
